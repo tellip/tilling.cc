fcd69dd4205e089a46423599b04fb7373b188cc2
ai-artisan <luojunyi.0@gmail.com>
2018-06-02 19:55:28 +0800

    asdf

--- archive/main.client.cc
+++
@@ -1,9 +0,0 @@
-#include "main.hh"
-
-int main(int argc, char *argv[]) {
-	for (auto i = 0; i < argc; i++) {
-		auto arg = argv[i];
-		if (strlen(arg) > 2 && arg[0] == '-' && arg[1] == '-' && arg[3] != '\n') wm::sendSock(std::string("c-") + &arg[2]);
-	}
-	return 0;
-};
