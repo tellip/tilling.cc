cc6e710108901f2e4e363e74f554c9c24907e2f7
ai-artisan <luojunyi.0@gmail.com>
2018-06-30 22:01:03 +0800

    asdf

--- src/server/main.cc
+++ src/server/main.cc
@@ -5,6 +5,9 @@ int main() {
         wm::matrix::matrix(display, break_, [&](const auto &command_handlers, const auto &root_event_mask, const auto &leaf_event_mask, const auto &event_handlers) {
             loop(command_handlers, root_event_mask, leaf_event_mask, event_handlers, [&](const auto &join) {
                 join();
+                system("if [ -f $HOME/.config/matwm/bash_exit ]; then\n"
+                       "\tbash $HOME/.config/matwm/bash_exit\n"
+                       "fi");
                 clean();
             });
         });
